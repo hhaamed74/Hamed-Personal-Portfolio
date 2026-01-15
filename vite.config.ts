66759e8d9f6bb96503dc6df163114847f998fd79
Hamed Abdul Mohsen <hamedabdulmohsenalsayed@gmail.com>
2026-01-15 03:12:15 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 1. تقسيم المكتبات لملفات صغيرة عشان السرعة
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    // 2. رفع حد التحذير لـ 1000 كيلوبايت لضمان عدم ظهور رسائل مزعجة
+    chunkSizeWarningLimit: 1000,
+  },
 });
